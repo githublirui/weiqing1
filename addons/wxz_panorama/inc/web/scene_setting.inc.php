fb0f25d528ea0d73641dd36325c1551979a5897b
lirui <649037629@qq.com>
2017-05-29 09:55:03 +0800

    版权设置

---
+++ addons/wxz_panorama/inc/web/scene_setting.inc.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * 场景相关配置
+ */
+require_once WXZ_PANORAMA . '/function/global.func.php';
+require_once WXZ_PANORAMA . '/source/Page.class.php';
+
+global $_W, $_GPC;
+
+$info = Page::getPage(array(10));
+
+if (checksubmit()) {
+    if (!$copyRight) {
+        //版权
+        $insertData = array(
+            'uniacid' => $_W['uniacid'],
+            'type' => 10,
+            'img' => $_GPC['copyright'],
+            'create_at' => time(),
+        );
+        pdo_insert(Page::$table, $insertData);
+    } else {
+        pdo_update(Page::$table, array('img' => $_GPC['copyright']), array('id' => $copyRight['id']));
+    }
+
+    message('保存成功', $this->createWebUrl('scene_setting'));
+}
+
+load()->web('tpl');
+include $this->template('web/scene_setting');
+?>
--- addons/wxz_shoppingmall/source/Page.class.php
+++ addons/wxz_shoppingmall/source/Page.class.php
@@ -21,63 +21,7 @@ class Page {
         7 => '注册页背景图(1080x830)',
         8 => '完善资料页背景图(1080x830)',
         9 => '商户首页导航',
-    );
-
-    /**
-     * svg图标
-     * @var type 
-     */
-    public static $index2PageNav = array(
-        'svg_icon' => array(
-            'icon_member_1' => '会员',
-            'icon_register_1' => '注册',
-            'icon_qiandao_1' => '签到',
-            'icon_balance_1' => '余额',
-            'icon_event_1' => '活动',
-            'icon_gift_1' => '礼物',
-            'icon_diyongquan_1' => '抵用券',
-            'icon_tuanGou_1' => '团购',
-            'icon_service_1' => '服务',
-            'icon_wifi_1' => 'Wifi',
-            'icon_tool_1' => '工具',
-            'icon_map_1' => '地图',
-            'icon_parking_1' => '停车',
-            'icon_floor_1' => '楼层',
-            'icon_shop_1' => '店铺',
-            'icon_shoppingCart_2' => '购物车',
-            'icon_office_1' => '办公楼',
-            'icon_hotel_1' => '酒店',
-            'icon_toilet_1' => '厕所',
-            'icon_food_1' => '美食',
-            'icon_child_1' => '儿童',
-            'icon_group_1' => '用户组',
-            'icon_kuaidi_1' => '快递',
-            'icon_didiDaChe_1' => '叫车',
-            'icon_star_3' => '五角星',
-            'icon_heka_1' => '贺卡',
-        ),
-        'color' => array(
-            'link1' => 'background:#009e42;',
-            'link2' => 'background:#e60021;',
-            'link3' => 'background:#00418e;',
-            'link4' => 'background:#21c063;',
-            'link5' => 'background:#ea156f;',
-            'link6' => 'background:#186acb;',
-            'link7' => 'background:#2bca6d;',
-            'link8' => 'background:#e3364f;',
-            'link9' => 'background:#0f5eae;',
-            'link10' => 'background:#3bd487;',
-            'link11' => 'background:#206cc8;',
-            'link12' => 'background:#e30e2a;',
-            'link13' => 'background:#b5384a;',
-            'link14' => 'background:#3484e1;',
-            'link15' => 'background:#2ebe67;',
-            'link16' => 'background:#0e58a8;',
-            'link17' => 'background:#186acb;',
-            'link18' => 'background:#e60021;',
-            'link19' => 'background:#3bd487;',
-            'link20' => 'background:#f55f01;',
-        ),
+//        10=> '场景版权'
     );
 
     /**
