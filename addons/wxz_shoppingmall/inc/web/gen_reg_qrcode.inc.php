7160cd9761786825f8c78a58455edde8ee5bbb1f
lirui <lirui@51talk.com>
2017-07-03 14:04:25 +0800

    批量生成二维码失败 重复生成

--- addons/wxz_shoppingmall/inc/web/gen_reg_qrcode.inc.php
+++ addons/wxz_shoppingmall/inc/web/gen_reg_qrcode.inc.php
@@ -6,6 +6,14 @@ $cacheKey = "wxz_shoppingmall_qrcode_num_{$_W['uniacid']}";
 set_time_limit(-1);
 $cache = cache_read($cacheKey);
 
+//$sql = "select count(1) from ims_wxz_shoppingmall_invite_code group by code having count(code)=1";
+//$ret = pdo_fetch($sql);
+//$sql = "select id,code from ims_wxz_shoppingmall_invite_code";
+//$ret = pdo_fetchall($sql);
+//foreach ($ret as $row) {
+//    echo $row['id'] . ',' . $randNum = sprintf('%08d', $row['code']) . ',' . date('Ymd') . '_' . $row['code'] . '.png' . "\n";
+//}
+//die;
 if ($cache) {
     $cacheData = $cache;
     if ($cacheData['num'] > 0) {
@@ -17,13 +25,20 @@ if ($cache) {
             //生成二维码
             $url = "{$_W['siteroot']}app/index.php?i={$_W['uniacid']}&c=entry&do=reg&m={$_GPC['m']}&code={$randNum}";
             $savePath = $cacheData['path'] . "/" . date('Ymd') . '_' . $randNum . '.png';
-            QRcode::png($url, $savePath, QR_ECLEVEL_Q, 4);
+            for ($try = 0; $try < 10; $try++) {
+                $ret = QRcode::png($url, $savePath, QR_ECLEVEL_Q, 4);
+                if (file_exists($savePath)) {
+                    break;
+                }
+            }
             $data = array(
                 'uniacid' => $_W['uniacid'],
                 'code' => $randNum,
                 'create_at' => time(),
             );
-            pdo_insert('wxz_shoppingmall_invite_code', $data);
+            if (file_exists($savePath)) {
+                pdo_insert('wxz_shoppingmall_invite_code', $data);
+            }
         }
         $cacheData['num'] = $cacheData['num'] - $runNum;
         if ($cacheData['num'] <= 0) {
