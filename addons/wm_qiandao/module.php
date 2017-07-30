b183f3527e3aea33a3e5f020c3f83ce7f6db7818
lirui <649037629@qq.com>
2017-07-31 05:36:16 +0800

    签到

---
+++ addons/wm_qiandao/module.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 签到累积分模块定义
+ *
+ * @author unrooter
+ * @url http://bbs.we7.cc/
+ */
+defined('IN_IA') or exit('Access Denied');
+
+class Wm_qiandaoModule extends WeModule {
+
+	public function settingsDisplay($settings) {
+		global $_W, $_GPC;
+		if(checksubmit()) {
+			$cfg = $settings;
+			$cfg['score'] = trim($_GPC['score']);
+			$cfg['field'] = trim($_GPC['field']);
+			if ($this->saveSettings($cfg)) {
+			    message('保存成功', 'refresh');
+			}
+		}
+		include $this->template('setting');
+	}
+
+}
