24344c861ad619c77e04e2890ea67502c981978f
lirui <649037629@qq.com>
2017-09-09 14:08:17 +0800

    商品扩展字段

--- addons/wxz_easy_pay/inc/mobile/orders.inc.php
+++ addons/wxz_easy_pay/inc/mobile/orders.inc.php
@@ -1,36 +1,31 @@
 <?php
-//print_R($_FILES);
+
 global $_GPC, $_W;
 checkauth();
-if($_W['fans']){
-	
-}else{
-	exit();
-}
 $uid = $_W['fans']['uid'];
+
+//$uid = 0;
 $sell_out = $_GPC['sell_out'];
-if($_GPC['ac']=='ajaxdele'){
-	$hangyi_product = pdo_get('hangyi_product', array('id' => $_GPC['id'],'uid'=>$_W['fans']['uid']));
-	if($hangyi_product['id']){
-		$re = pdo_query("DELETE FROM ".tablename('hangyi_product')."  WHERE id = '".$hangyi_product['id']."' limit 1");
-		if($re){
-			echo 1;
-		}
-	}
-	exit();
+if ($_GPC['ac'] == 'ajaxdele') {
+    $hangyi_product = pdo_get('hangyi_product', array('id' => $_GPC['id'], 'uid' => $_W['fans']['uid']));
+    if ($hangyi_product['id']) {
+        $re = pdo_query("DELETE FROM " . tablename('hangyi_product') . "  WHERE id = '" . $hangyi_product['id'] . "' limit 1");
+        if ($re) {
+            echo 1;
+        }
+    }
+    exit();
 }
-if($sell_out){
-	$sql = 'SELECT * FROM ' . tablename('hangyi_product')." WHERE uid = :uid and (`goodsStock` < 1  or `goodsStock`='' or `goodsStock` is NULL)order by id desc";
-}else{
-	$sql = 'SELECT * FROM ' . tablename('hangyi_product')." WHERE uid = :uid and `goodsStock` > 0 order by id desc";
+if ($sell_out) {
+    $sql = 'SELECT * FROM ' . tablename('hangyi_product') . " WHERE uid = :uid and (`goodsStock` < 1  or `goodsStock`='' or `goodsStock` is NULL)order by id desc";
+} else {
+    $sql = 'SELECT * FROM ' . tablename('hangyi_product') . " WHERE uid = :uid and `goodsStock` > 0 order by id desc";
 }
 
 $pars[':uid'] = $uid;
 
-
 $trades_p = pdo_fetchall($sql, $pars);
 include $this->template('orders');
-
 ?>
 
 
