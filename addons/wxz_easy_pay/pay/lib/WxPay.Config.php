ffbf6b59e7c890bdcea08c0ba3cb2b698f55b3b7
lirui <lirui@51talk.com>
2017-09-16 17:50:48 +0800

    证书路径PHP 兼容

--- addons/wxz_easy_pay/pay/lib/WxPay.Config.php
+++ addons/wxz_easy_pay/pay/lib/WxPay.Config.php
@@ -36,8 +36,8 @@ class WxPayConfig
 	 * API证书下载地址：https://pay.weixin.qq.com/index.php/account/api_cert，下载之前需要安装商户操作证书）
 	 * @var path
 	 */
-	const SSLCERT_PATH = IA_ROOT."/addons/wxz_easy_pay/pay/cert/apiclient_cert.pem";
-	const SSLKEY_PATH = IA_ROOT."/addons/wxz_easy_pay/pay/cert/apiclient_key.pem";
+	const SSLCERT_PATH = "/addons/wxz_easy_pay/pay/cert/apiclient_cert.pem";
+	const SSLKEY_PATH =  "/addons/wxz_easy_pay/pay/cert/apiclient_key.pem";
 	
 	//=======【curl代理设置】===================================
 	/**
