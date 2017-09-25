c60f9d34353ed8f143576427918c6fdfd017bd2f
lirui <649037629@qq.com>
2017-09-26 07:27:29 +0800

    多商品，订单详情展示

--- addons/wxz_easy_pay/inc/mobile/ordersdetail.inc.php
+++ addons/wxz_easy_pay/inc/mobile/ordersdetail.inc.php
@@ -1,26 +1,38 @@
 <?php
-//print_R($_FILES);
+
 global $_GPC, $_W;
 checkauth();
-if($_W['fans']){
-	
-}else{
-	exit();
+if ($_W['fans']) {
+    
+} else {
+    message('用户授权错误', $this->createMobileUrl('my'));
 }
-if(empty($_GPC['oid'])){
-	exit();
+
+
+if (empty($_GPC['oid'])) {
+    message('订单不存在', $this->createMobileUrl('my'));
 }
-$oid = (int)$_GPC['oid'];
-$orderinfo = pdo_get('hangyi_order', array('id' => $oid,'sell_openid'=>$_W['fans']['openid']));
-if($orderinfo){
-	$buy_info = pdo_get('hangyi_user', array('uid' => $orderinfo['buy_id']));
-	$product_info = pdo_get('hangyi_product', array('id' => $orderinfo['pid']));
-}else{
-	exit();
+
+$oid = (int) $_GPC['oid'];
+$orderinfo = pdo_get('hangyi_order', array('id' => $oid, 'sell_openid' => $_W['fans']['openid']));
+if ($orderinfo) {
+    $buy_info = pdo_get('hangyi_user', array('uid' => $orderinfo['buy_id']));
+    $product_info = pdo_get('hangyi_product', array('id' => $orderinfo['pid']));
+    
+    $productIds = explode(',', $orderinfo['pids']);
+    $goodnums = explode(',', $orderinfo['goodsNums']);
+    
+    foreach($productIds as $productId) {
+        $product_infos[]  = pdo_get('hangyi_product', array('id' => $productId));
+    }
+    
+} else {
+    message('订单不存在', $this->createMobileUrl('my'));
 }
+
+
 
 include $this->template('orderdetail');
-
 ?>
 
 
