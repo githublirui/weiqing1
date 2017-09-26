586414718ddf4fb02ae990d0e47df7ec8852f9e7
lirui <649037629@qq.com>
2017-09-27 02:30:33 +0800

    分销商发货

--- addons/wxz_easy_pay/inc/mobile/ordersdetail.inc.php
+++ addons/wxz_easy_pay/inc/mobile/ordersdetail.inc.php
@@ -14,19 +14,47 @@ if (empty($_GPC['oid'])) {
 }
 
 $oid = (int) $_GPC['oid'];
+$getcode = (string) $_GPC['code'];
+
 $orderinfo = pdo_get('hangyi_order', array('id' => $oid, 'sell_openid' => $_W['fans']['openid']));
 if ($orderinfo) {
-    $buy_info = pdo_get('hangyi_user', array('uid' => $orderinfo['buy_id']));
+
+    $key = "wxz_easy_pay_{$_GPC['oid']}";
+    $auth = 123456;
+    require_once WXZ_EASY_PAY . '/function/global.func.php';
+    $code = authcode($auth, '', $key);
+    $shareUrl = $this->createMobileUrl('ordersdetail', array('oid' => $oid, 'code' => $code));
+    $shareUrl = $_W['siteroot'] . 'app' . ltrim($shareUrl, '.');
+
+    if ($orderinfo['sell_id'] != $_W['fans']['uid']) {
+        //检测权限,分销商发货
+        if (!$code) {
+            message('无权限查看', $this->createMobileUrl('my'));
+        }
+
+        $decode = authcode($getcode, 'DECODE', $key);
+        if ($decode != $auth) {
+            message('无权限查看1', $this->createMobileUrl('my'));
+        }
+    }
+
+    //买家信息
+    if ($orderinfo['buy_id']) {
+        $buyInfo = pdo_get('hangyi_user', array('uid' => $orderinfo['buy_id']));
+    }
+    if ($orderinfo['sell_id']) {
+        $sellInfo = pdo_get('hangyi_user', array('uid' => $orderinfo['sell_id']));
+    }
+
     $product_info = pdo_get('hangyi_product', array('id' => $orderinfo['pid']));
-    
+
     $productIds = explode(',', $orderinfo['pids']);
     $goodnums = explode(',', $orderinfo['goodsNums']);
     $attrs = explode(',', $orderinfo['attrs']);
-    
-    foreach($productIds as $productId) {
-        $product_infos[]  = pdo_get('hangyi_product', array('id' => $productId));
+
+    foreach ($productIds as $productId) {
+        $product_infos[] = pdo_get('hangyi_product', array('id' => $productId));
     }
-    
 } else {
     message('订单不存在', $this->createMobileUrl('my'));
 }
